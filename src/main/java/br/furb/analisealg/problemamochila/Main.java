43673de0d04343ffe143bbd23aa69cbc368667af
Leeonardoo <leeonardo.deo@gmail.com>
2022-05-06 00:01:07 -0300

    Adicionado métodos recursivos e dinâmico bottom-up

--- src/main/java/br/furb/analisealg/problemamochila/Main.java
+++ src/main/java/br/furb/analisealg/problemamochila/Main.java
@@ -1,10 +1,8 @@
 package br.furb.analisealg.problemamochila;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
@@ -20,26 +18,90 @@ public class Main {
         }
 
         for (File file : inputFiles) {
-            var result = loadFile(file);
+            Mochila result = lerMochila(file);
+            int n = result.getItems()[0].length;
+            int[] v = result.getItems()[1];
+            int[] w = result.getItems()[0];
+
+            System.out.println(mochilaRecursivo(n, v, w, result.getW()));
+            System.out.println(mochilaDinamicaBottomUp(n, v, w, result.getW()));
         }
     }
 
-    private static int[][] loadFile(File file) throws Exception {
-        int[][] mArr;
+    private static Mochila lerMochila(File file) throws Exception {
+        int[][] items;
+
+        Path path = file.toPath();
+        items = new int[2][(int) Files.lines(path).count() - 1];
 
         BufferedReader br = new BufferedReader(new FileReader(file));
-        int capacity = Integer.parseInt(br.readLine());
+        Mochila mochila = new Mochila(Integer.parseInt(br.readLine()));
+        int i = 0;
 
-        List<Item> items = br
-                .lines()
-                .map((line) -> {
-                    String[] values = line.split(" ");
-                    return new Item(Integer.parseInt(values[0]), Integer.parseInt(values[1]));
-                })
-                .collect(Collectors.toList());
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(" ");
 
-        mArr = new int[items.size()][capacity];
+            items[0][i] = Integer.parseInt(values[0]);
+            items[1][i] = Integer.parseInt(values[1]);
+            i++;
+        }
 
-        return mArr;
+        mochila.setItems(items);
+        return mochila;
+    }
+
+    /**
+     * @param n item atual (começa em .length)
+     * @param v Array de valores
+     * @param w Array de pesos
+     * @param W Capacidade da bolsa
+     */
+    private static int mochilaRecursivo(int n, int[] v, int[] w, int W) {
+        if (n == 0 || W == 0)
+            return 0;
+
+        if (w[n - 1] > W) {
+            return mochilaRecursivo(n - 1, v, w, W);
+        } else {
+            int usa = v[n - 1] + mochilaRecursivo(n - 1, v, w, W - w[n - 1]);
+            int naoUsa = mochilaRecursivo(n - 1, v, w, W);
+
+            return Math.max(usa, naoUsa);
+        }
+    }
+
+    /**
+     * @param n item atual (começa em .length)
+     * @param v Array de valores
+     * @param w Array de pesos
+     * @param W Capacidade da bolsa
+     */
+    private static int mochilaDinamicaBottomUp(int n, int[] v, int[] w, int W) {
+        int[][] M = new int[n + 1][W + 1];
+
+        /* Já é inicializado com 0 no java
+        for (int X = 0; X <= W; X++) {
+            M[0][X] = 0;
+        }
+
+        for (int j = 0; j <= n; j++) {
+            M[j][0] = 0;
+        }*/
+
+        //-1 no j pois acessa os outros arrays que tem uma linha e coluna a menos
+        for (int j = 1; j <= n; j++) {
+            for (int X = 0; X <= W; X++) {
+                if (w[j - 1] > X) {
+                    M[j - 1][X] = M[j - 1][X];
+                } else {
+                    int usa = v[j - 1] + M[j - 1][X - w[j - 1]]; //valor de uma posição qualquer da linha –1
+                    int naoUsa = M[j - 1][X]; //item da linha anterior
+                    M[j][X] = Math.max(usa, naoUsa);
+                }
+            }
+        }
+
+        return M[n][W];
     }
 }
---
+++ src/main/java/br/furb/analisealg/problemamochila/Mochila.java
@@ -0,0 +1,31 @@
+package br.furb.analisealg.problemamochila;
+
+public class Mochila {
+    int W;
+    int[][] items;
+
+    public Mochila(int W, int[][] items) {
+        this.W = W;
+        this.items = items;
+    }
+
+    public Mochila(int W) {
+        this.W = W;
+    }
+
+    public int getW() {
+        return W;
+    }
+
+    public void setW(int W) {
+        this.W = W;
+    }
+
+    public int[][] getItems() {
+        return items;
+    }
+
+    public void setItems(int[][] items) {
+        this.items = items;
+    }
+}
